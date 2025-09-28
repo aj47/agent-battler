a077a637dde12d5f033229e96c394a74d146acc7
AJ <yspdev@gmail.com>
2025-09-28 12:31:52 -0700

    chore: prepare for Vercel deployment

---
+++ .open-next/dynamodb-provider/open-next.config.mjs
@@ -0,0 +1,17 @@
+import { createRequire as topLevelCreateRequire } from 'module';const require = topLevelCreateRequire(import.meta.url);import bannerUrl from 'url';const __dirname = bannerUrl.fileURLToPath(new URL('.', import.meta.url));
+
+// open-next.config.ts
+var open_next_config_default = {
+  default: {
+    override: {
+      wrapper: "cloudflare",
+      converter: "edge",
+      incrementalCache: "dummy",
+      tagCache: "dummy",
+      queue: "dummy"
+    }
+  }
+};
+export {
+  open_next_config_default as default
+};
